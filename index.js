78b28819dd9a9e187f56e6772b1531678de5e50d
marufbroh <marufmd775@gmail.com>
2023-05-07 18:03:29 +0600

    server final comit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const users = require("./data/usersData");
+
+app.get("/", (req, res) => {
+  res.send("Users management server is running");
+});
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
+});
+
+app.listen(port, () => {
+  console.log(`server is running on PORT ${port}}`);
+});
